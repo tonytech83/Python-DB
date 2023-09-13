afa73653dfee124054a4b99064c698442ea0d794
Anton Petrov <a.petrov.83@gmail.com>
2023-09-13 14:51:34 +0300

    added solution

---
+++ 01_PostgreSQL/01_Data_Types_and_Table_Basics/01_Lab/01_create_tables.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE gamebar;
+
+CREATE TABLE employees (
+	id SERIAL PRIMARY KEY NOT NULL,
+	first_name VARCHAR(30),
+	last_name VARCHAR(50),
+	hiring_date DATE DEFAULT '2023-01-01',
+	salary NUMERIC(10,2),
+	devices_number INT
+);
+
+CREATE TABLE departments (
+	id SERIAL PRIMARY KEY NOT NULL,
+	name VARCHAR(50),
+	code CHAR(3),
+	description TEXT
+);
+
+CREATE TABLE issues (
+	id SERIAL PRIMARY KEY UNIQUE,
+	description VARCHAR(150),
+	date DATE,
+	START TIMESTAMP
+);
