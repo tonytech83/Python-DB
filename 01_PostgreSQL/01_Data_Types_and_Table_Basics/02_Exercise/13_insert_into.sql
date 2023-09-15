26294a0865705f71d8e94619c848518b8174d829
Anton Petrov <a.petrov.83@gmail.com>
2023-09-15 11:19:33 +0300

    added solution

---
+++ 01_PostgreSQL/01_Data_Types_and_Table_Basics/02_Exercise/13_insert_into.sql
@@ -0,0 +1,9 @@
+INSERT INTO minions_info (name, code, task, banana, email, equipped, mood)
+VALUES ('Mark', 'GKYA', 'Graphing Points', 3265.265, 'mark@minion.com', FALSE, 'happy'),
+       ('Mel', 'HSK', 'Science Investigation', 54784.996, 'mel@minion.com', TRUE, 'stressed'),
+       ('Bob', 'HF', 'Painting', 35.652, 'bob@minion.com', TRUE, 'happy'),
+       ('Darwin', 'EHND', 'Create a Digital Greeting', 321.958, 'darwin@minion.com', FALSE, 'relaxed'),
+       ('Kevin', 'KMHD', 'Construct with Virtual Blocks', 35214.789, 'kevin@minion.com', FALSE, 'happy'),
+       ('Norbert', 'FEWB', 'Testing', 3265.5, 'norbert@minion.com', TRUE, 'sad'),
+       ('Donny', 'L', 'Make a Map', 8.452, 'donny@minion.com', TRUE, 'happy');
+
---
+++ 01_PostgreSQL/01_Data_Types_and_Table_Basics/02_Exercise/14_select.sql
@@ -0,0 +1,5 @@
+SELECT mi.name,
+       mi.task,
+       mi.email,
+       mi.banana
+FROM minions_info AS mi;
