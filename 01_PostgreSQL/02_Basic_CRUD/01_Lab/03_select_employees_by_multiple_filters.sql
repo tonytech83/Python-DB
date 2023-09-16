9801dcc2fdea73292949784530b635ac20298a9f
Anton Petrov <a.petrov.83@gmail.com>
2023-09-16 09:47:53 +0300

    added solution

---
+++ 01_PostgreSQL/02_Basic_CRUD/01_Lab/03_select_employees_by_multiple_filters.sql
@@ -0,0 +1,9 @@
+SELECT id,
+       first_name,
+       last_name,
+       job_title,
+       department_id,
+       salary
+FROM employees
+WHERE salary > 1000.00 AND department_id = 4
+ORDER BY id;
