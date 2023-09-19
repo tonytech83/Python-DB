3491eb1c7e3b6baab5791d35b19ee8f928729938
Anton Petrov <a.petrov.83@gmail.com>
2023-09-19 15:35:41 +0300

    update solution with second solution

--- 01_PostgreSQL/02_Basic_CRUD/02_Exercise/24_substring.sql
+++ 01_PostgreSQL/02_Basic_CRUD/02_Exercise/24_substring.sql
@@ -7,3 +7,11 @@ SELECT LEFT(first_name, 2) AS initial,
 FROM employees
 ORDER BY last_name;
 
+
+-- Same -> using SUBSTRING instead of LEFT
+-- CREATE VIEW view_initials AS
+-- SELECT SUBSTRING(first_name, 1,2) AS initial,
+--        last_name
+-- FROM employees
+-- ORDER BY last_name;
+
