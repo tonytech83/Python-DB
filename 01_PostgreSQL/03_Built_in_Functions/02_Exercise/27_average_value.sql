266ee04cc39edbca342421ad581568ec3c1aa94d
Anton Petrov <a.petrov.83@gmail.com>
2023-09-22 10:53:14 +0300

    upate solution

--- 01_PostgreSQL/03_Built_in_Functions/02_Exercise/27_average_value.sql
+++ 01_PostgreSQL/03_Built_in_Functions/02_Exercise/27_average_value.sql
@@ -1,6 +1,6 @@
 -- 27. Average Value
 -- url: https://judge.softuni.org/Contests/Compete/Index/4105#24
 
-SELECT avg(multiplication)
+SELECT avg(multiplication) AS average_value
 FROM bookings_calculation;
 
