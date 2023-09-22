9cdd31e207a6e4b9b16cfecba1c6a6731521da87
Anton Petrov <a.petrov.83@gmail.com>
2023-09-22 10:50:25 +0300

    upate solution

--- 01_PostgreSQL/03_Built_in_Functions/02_Exercise/25_count_by_initial.sql
+++ 01_PostgreSQL/03_Built_in_Functions/02_Exercise/25_count_by_initial.sql
@@ -1,9 +1,16 @@
 -- 25. COUNT by Initial
 -- url: https://judge.softuni.org/Contests/Compete/Index/4105#22
 
-SELECT substring(first_name, 1, 2) AS initials,
-       count(first_name)           AS user_count
+SELECT left(first_name, 2) AS initials,
+       count(id)           AS user_count
 FROM users
 GROUP BY initials
 ORDER BY user_count DESC, initials;
+
+-- Other way of solution
+-- SELECT left(first_name, 2) AS initials,
+--        count('initials')   AS user_count
+-- FROM users
+-- GROUP BY initials
+-- ORDER BY user_count DESC, initials;
 
