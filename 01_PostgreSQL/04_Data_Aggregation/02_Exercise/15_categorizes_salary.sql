5a5936baf3e084f563bbd6781bf34e86830467c9
Anton Petrov <a.petrov.83@gmail.com>
2023-09-25 20:17:41 +0300

    added solution

---
+++ 01_PostgreSQL/04_Data_Aggregation/02_Exercise/15_categorizes_salary.sql
@@ -0,0 +1,12 @@
+-- 15. Categorizes Salary
+-- url: https://judge.softuni.org/Contests/Compete/Index/4107#14
+
+SELECT job_title,
+       CASE
+           WHEN avg(salary) > 45800 THEN 'Good'
+           WHEN avg(salary) BETWEEN 27500 AND 45800 THEN 'Medium'
+           WHEN avg(salary) < 27500 THEN 'Need Improvement'
+           END AS "Category"
+FROM employees
+GROUP BY job_title
+ORDER BY "Category", job_title;
