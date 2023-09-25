0d40f4363e5785f9c17d323cd4c245a2265780c9
Anton Petrov <a.petrov.83@gmail.com>
2023-09-25 19:03:38 +0300

    added solution

---
+++ 01_PostgreSQL/04_Data_Aggregation/02_Exercise/04_min_deposit_charge.sql
@@ -0,0 +1,5 @@
+-- 04. MIN Deposit Charge
+-- url: https://judge.softuni.org/Contests/Compete/Index/4107#3
+
+SELECT min(deposit_charge) AS "Minimum Deposit Charge"
+FROM wizard_deposits;
---
+++ 01_PostgreSQL/04_Data_Aggregation/02_Exercise/05_max_age.sql
@@ -0,0 +1,5 @@
+-- 05. MAX Age
+-- url: https://judge.softuni.org/Contests/Compete/Index/4107#4
+
+SELECT max(age) AS "Maximum Age"
+FROM wizard_deposits;
