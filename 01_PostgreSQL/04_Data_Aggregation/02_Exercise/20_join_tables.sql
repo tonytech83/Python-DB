cf86cf057023912d5ea7f4c5c6aa39dd4d211670
Anton Petrov <a.petrov.83@gmail.com>
2023-09-26 15:14:30 +0300

    updated solution

--- 01_PostgreSQL/04_Data_Aggregation/02_Exercise/20_join_tables.sql
+++ 01_PostgreSQL/04_Data_Aggregation/02_Exercise/20_join_tables.sql
@@ -1,17 +1,6 @@
 -- 20. JOIN Tables
 -- url: https://judge.softuni.org/Contests/Compete/Index/4107#19
 
-SELECT d.id,
-       d.department_name,
-       d.manager_id,
-       e.id,
-       e.first_name,
-       e.last_name,
-       e.job_title,
-       e.department_id,
-       e.manager_id,
-       e.hire_date,
-       e.salary,
-       e.address_id
-FROM departments d
-         JOIN employees e on d.id = e.department_id;
+SELECT *
+FROM departments
+         JOIN employees ON departments.id = employees.department_id;
