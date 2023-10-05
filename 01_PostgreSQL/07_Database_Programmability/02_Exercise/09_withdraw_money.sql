16c2c34e8555e58fc21faf0cbcd237035d46e361
Anton Petrov <a.petrov.83@gmail.com>
2023-10-05 15:13:57 +0300

    update solution with raise a notise when insufficient balance to withdraw

--- 01_PostgreSQL/07_Database_Programmability/02_Exercise/09_withdraw_money.sql
+++ 01_PostgreSQL/07_Database_Programmability/02_Exercise/09_withdraw_money.sql
@@ -9,12 +9,15 @@ AS
 $$
 DECLARE
     account_balance NUMERIC;
+    insufficient_balance NUMERIC;
 BEGIN
     IF (SELECT balance FROM accounts WHERE id = account_id) IS NULL THEN
         RETURN;
     ELSE
         account_balance := (SELECT balance FROM accounts WHERE id = account_id);
         IF account_balance < money_amount THEN
+            insufficient_balance := round((money_amount - account_balance), 4);
+            RAISE NOTICE 'Insufficient balance to withdraw %', insufficient_balance;
             RETURN;
         ELSE
             UPDATE accounts SET balance = balance - money_amount WHERE id = account_id;
