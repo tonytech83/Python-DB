5128146013b5018da741f7376719328e1fc043e7
Anton Petrov <a.petrov.83@gmail.com>
2023-10-22 17:22:57 +0300

    Added solution for exam "05. Product"

--- 02_Python_ORM/01_Django_Models_Basics/02_Exercise/main_app/models.py
+++ 02_Python_ORM/01_Django_Models_Basics/02_Exercise/main_app/models.py
@@ -25,3 +25,10 @@ class Recipe(models.Model):
     ingredients = models.TextField()
     cook_time = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=70)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
