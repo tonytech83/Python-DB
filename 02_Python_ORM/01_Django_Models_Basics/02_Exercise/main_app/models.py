7555442ce716a6945f8735e9b5a3970f02c6240d
Anton Petrov <a.petrov.83@gmail.com>
2023-10-22 17:32:07 +0300

    Added solution for exam "07. Exercise"

--- 02_Python_ORM/01_Django_Models_Basics/02_Exercise/main_app/models.py
+++ 02_Python_ORM/01_Django_Models_Basics/02_Exercise/main_app/models.py
@@ -42,3 +42,14 @@ class UserProfile(models.Model):
     bio = models.TextField(max_length=120)
     profile_image_url = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    difficulty_level = models.CharField(max_length=20)
+    duration_minutes = models.PositiveIntegerField()
+    equipment = models.CharField(max_length=90)
+    video_url = models.URLField(null=True, blank=True)
+    calories_burned = models.PositiveIntegerField(default=0)
+    is_favorite = models.BooleanField(default=False)
