ccdd63b9324925685b910a36e4b2fe94b163806e
Anton Petrov <a.petrov.83@gmail.com>
2023-10-22 17:27:25 +0300

    Added solution for exam "06. User Profile"

--- 02_Python_ORM/01_Django_Models_Basics/02_Exercise/main_app/models.py
+++ 02_Python_ORM/01_Django_Models_Basics/02_Exercise/main_app/models.py
@@ -32,3 +32,13 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class UserProfile(models.Model):
+    username = models.CharField(max_length=65, unique=True)
+    first_name = models.CharField(max_length=40, unique=True)
+    last_name = models.CharField(max_length=40, unique=True)
+    email = models.EmailField(unique=True, default="students@softuni.bg")
+    bio = models.TextField(max_length=120)
+    profile_image_url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
