ee5cb77a61deb79f81807f1634981218f79cc5e3
Anton Petrov <a.petrov.83@gmail.com>
2023-10-22 17:50:01 +0300

    Added solution for exam "08. Book"

--- 02_Python_ORM/01_Django_Models_Basics/02_Exercise/main_app/models.py
+++ 02_Python_ORM/01_Django_Models_Basics/02_Exercise/main_app/models.py
@@ -53,3 +53,24 @@ class Exercise(models.Model):
     video_url = models.URLField(null=True, blank=True)
     calories_burned = models.PositiveIntegerField(default=0)
     is_favorite = models.BooleanField(default=False)
+
+
+class Book(models.Model):
+    GENRES = [
+        ('Fiction', 'Fiction'),
+        ('Non-Fiction', 'Non-Fiction'),
+        ('Science Fiction', 'Science Fiction'),
+        ('Horror', 'Horror')
+    ]
+
+    title = models.CharField(max_length=30)
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=20, choices=GENRES)
+    publication_date = models.DateField(editable=False, auto_now_add=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    rating = models.FloatField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
