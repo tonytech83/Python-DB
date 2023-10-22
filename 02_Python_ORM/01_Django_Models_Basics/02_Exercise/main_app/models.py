d9449e28f397f9681aa148585805e1c248524e37
Anton Petrov <a.petrov.83@gmail.com>
2023-10-22 17:09:27 +0300

    Added solution for exam "02. Blog"

--- 02_Python_ORM/01_Django_Models_Basics/02_Exercise/main_app/models.py
+++ 02_Python_ORM/01_Django_Models_Basics/02_Exercise/main_app/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=30)
     age = models.PositiveIntegerField()
+
+
+class Blog(models.Model):
+    post = models.TextField()
+    author = models.CharField(max_length=35)
