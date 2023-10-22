4b174583d6e210e3166c22405a0bbf9894de4258
Anton Petrov <a.petrov.83@gmail.com>
2023-10-22 17:18:48 +0300

    Added solution for exam "04. Recipe"

--- 02_Python_ORM/01_Django_Models_Basics/02_Exercise/main_app/models.py
+++ 02_Python_ORM/01_Django_Models_Basics/02_Exercise/main_app/models.py
@@ -17,3 +17,11 @@ class WeatherForecast(models.Model):
     temperature = models.FloatField()
     humidity = models.FloatField()
     precipitation = models.FloatField()
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    ingredients = models.TextField()
+    cook_time = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
