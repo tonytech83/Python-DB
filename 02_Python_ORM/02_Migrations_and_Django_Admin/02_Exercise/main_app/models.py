c533c9287f927fd1bb7871a917ea27403d29893d
Anton Petrov <a.petrov.83@gmail.com>
2023-10-30 20:11:28 +0200

    Added solution for exams 10

--- 02_Python_ORM/02_Migrations_and_Django_Admin/02_Exercise/main_app/models.py
+++ 02_Python_ORM/02_Migrations_and_Django_Admin/02_Exercise/main_app/models.py
@@ -154,3 +154,46 @@ class Smartphone(models.Model):
         max_length=20,
         default="empty"
     )
+
+
+# Exam: 10.	Order
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('P', 'Pending'),
+        ('Cmd', 'Completed'),
+        ('Cnd', 'Cancelled'),
+    ]
+
+    product_name = models.CharField(
+        max_length=30,
+    )
+    customer_name = models.CharField(
+        max_length=100,
+    )
+    order_date = models.DateField()
+    status = models.CharField(
+        max_length=30,
+        choices=STATUS_CHOICES,
+    )
+    amount = models.PositiveIntegerField(
+        default=1
+    )
+    product_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+    total_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0,
+    )
+    warranty = models.CharField(
+        default="No warranty",
+    )
+    delivery = models.DateField(
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return f"Order #{self.pk} - {self.customer_name}"
