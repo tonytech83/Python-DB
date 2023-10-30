bb7741ee3124ed800fcc1e8ce7c6add930553152
Anton Petrov <a.petrov.83@gmail.com>
2023-10-30 16:15:02 +0200

    Added solution for exams form 2 to 6 from "Migrations and Django Admin - Exercise"

---
+++ 02_Python_ORM/02_Migrations_and_Django_Admin/02_Exercise/main_app/admin.py
@@ -0,0 +1,66 @@
+from django.contrib import admin
+
+from main_app.models import EventRegistration, Movie, Student, Supplier, Course
+
+
+# Exam: 02. Event Registration
+@admin.register(EventRegistration)
+class EventRegistrationAdmin(admin.ModelAdmin):
+    list_display = ('event_name', 'participant_name', 'registration_date')
+    list_filter = ('event_name', 'registration_date')
+    search_fields = ('event_name', 'participant_name')
+
+
+# Exam: 03. Movie
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("title", "director", "release_year", "genre")
+    list_filter = ("release_year", "genre")
+    search_fields = ("title", "director")
+
+
+# Exam: 04. Student
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "age", "grade")
+    list_filter = ("age", "grade", "date_of_birth")
+    search_fields = ('first_name',)
+    fieldsets = (
+        ('Personal Information', {
+            'fields': ("first_name", "last_name", "age", "date_of_birth")
+        }),
+        ('Academic Information', {
+            'fields': ('grade',)
+        })
+    )
+
+
+# Exam: 05. Supplier
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "phone")
+    list_filter = ("name", "phone")
+    search_fields = ("email", "contact_person", "phone")
+    list_per_page = 20
+    fieldsets = (
+        ('Information', {
+            'fields': ("name", "contact_person", "email", "address")
+        }),
+    )
+
+
+# Exam: 06. Course
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'lecturer', 'price', 'start_date')
+    list_filter = ('is_published', 'lecturer')
+    search_fields = ('title', 'lecturer')
+    fieldsets = (
+        ('Course Information', {
+            'fields': ('title', 'lecturer', 'price', 'start_date', 'is_published')
+        }),
+        ('Description', {
+            'fields': ('description',)
+        })
+    )
+    readonly_fields = ('start_date',)
