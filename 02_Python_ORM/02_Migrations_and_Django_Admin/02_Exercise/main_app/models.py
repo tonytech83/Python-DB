3af2ab302ab0c73b9e2ee357e8a972ff6a22d546
Anton Petrov <a.petrov.83@gmail.com>
2023-10-30 17:11:40 +0200

    Added solution for exams 8

--- 02_Python_ORM/02_Migrations_and_Django_Admin/02_Exercise/main_app/models.py
+++ 02_Python_ORM/02_Migrations_and_Django_Admin/02_Exercise/main_app/models.py
@@ -120,3 +120,21 @@ class Person(models.Model):
 
     def __str__(self):
         return f"Name: {self.name}"
+
+
+# Exam: 08.	Item
+class Item(models.Model):
+    name = models.CharField(
+        max_length=100,
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+    quantity = models.PositiveIntegerField(
+        default=1,
+    )
+    rarity = models.CharField(
+        max_length=20,
+        default='empty'
+    )
