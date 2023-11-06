375e1d7c83cab0cf5c76f00a93b7da0199320706
Anton Petrov <a.petrov.83@gmail.com>
2023-11-06 19:46:18 +0200

    Added solution for problem "01. Character Classes" from "Models Inheritance and Customization - Exercise"

---
+++ 02_Python_ORM/06_Models_Inheritance_and_Customization/02_Exercise/caller.py
@@ -0,0 +1,32 @@
+import os
+import django
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+from main_app.models import BaseCharacter, Mage, Necromancer
+
+#
+# Exam: 01. Character Classes
+# Test Code
+#
+# Create instances
+# mage = Mage.objects.create(
+#     name="Fire Mage",
+#     description="A powerful mage specializing in fire magic.",
+#     elemental_power="Fire",
+#     spellbook_type="Ancient Grimoire"
+# )
+#
+# necromancer = Necromancer.objects.create(
+#     name="Dark Necromancer",
+#     description="A mage specializing in dark necromancy.",
+#     elemental_power="Darkness", spellbook_type="Necronomicon",
+#     raise_dead_ability="Raise Undead Army"
+# )
+# print(mage.elemental_power)
+# print(mage.spellbook_type)
+# print(necromancer.name)
+# print(necromancer.description)
+# print(necromancer.raise_dead_ability)
